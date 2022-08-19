1bb59cd1169b25620623d004bc6bb74927a9fb89
Abdan Hurmuzi <abdan.idza2345@gmail.com>
2022-08-19 22:55:29 +0700

    commit

--- controllers/paymentHandling.js
+++ controllers/paymentHandling.js
@@ -3,6 +3,12 @@ const { Transaction } = require("../model");
 const paymentHandling = async (responseData) => {
     try {
         console.log(responseData.order_id);
+        const regPat = /responseData.order_id/;
+        console.log(regPat);
+        const transaction = await Transaction.find({
+            order_id: regPat
+        })
+        console.log(transaction);
         return;
     } catch (error) {
         
